118f5fdc9fd7f54bf62f2ecadf96b7b03577915a
Shwetanshu13 <shwetanshusinha13@gmail.com>
2025-12-12 02:58:18 +0530

    Removed redundant app folder

--- apps/inventory/inventory-frontend/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Set workspace root to silence monorepo warning
-    turbo: {
-        root: '../../..'
-    }
-};
-
-export default nextConfig;
